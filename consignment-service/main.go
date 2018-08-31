84287493f06145e048fb6437aef16ced53a45836
Chad Thomas <chad.c.thomas@gmail.com>
2018-08-31 09:17:16 -0400

    Update consignment service with neeedful comments

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -11,29 +11,29 @@ import (
 	"golang.org/x/net/context"
 )
 
+// The Repository type is an interface for the ConsignmentRepository
 type Repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
 	GetAll() []*pb.Consignment
 }
 
+// The ConsignmentRepository type is a repository for Consignments
 type ConsignmentRepository struct {
 	consignments []*pb.Consignment
 }
 
+// Create a Consignment in the repository
 func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
 }
 
+// GetAll Consignments in the repository
 func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
 
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
 type service struct {
 	repo         Repository
 	vesselClient vesselProto.VesselServiceClient
