92db7feaa40194c08ba64ade64470c0ab42f83ab
Chad Thomas <chad.c.thomas@gmail.com>
2018-09-10 09:24:24 -0400

    refactor main to use repo and datastore

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -4,85 +4,44 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	micro "github.com/micro/go-micro"
 	pb "github.com/skyarkitekten/go-microservices/consignment-service/proto/consignment"
 	vesselProto "github.com/skyarkitekten/go-microservices/vessel-service/proto/vessel"
-	"golang.org/x/net/context"
 )
 
-// The Repository type is an interface for the ConsignmentRepository
-type Repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-// The ConsignmentRepository type is a repository for Consignments
-type ConsignmentRepository struct {
-	consignments []*pb.Consignment
-}
-
-// Create a Consignment in the repository
-func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-// GetAll Consignments in the repository
-func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-type service struct {
-	repo         Repository
-	vesselClient vesselProto.VesselServiceClient
-}
-
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
-		MaxWeight: req.Weight,
-		Capacity:  int32(len(req.Containers)),
-	})
-	log.Printf("found a vessel: %s \n", vesselResponse.Vessel.Name)
-	if err != nil {
-		return err
-	}
-
-	req.VesselId = vesselResponse.Vessel.Id
-
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "localhost:27017"
+)
 
 func main() {
 
-	repo := &ConsignmentRepository{}
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = defaultHost
+	}
 
-	srv := micro.NewService(
+	session, err := CreateSession(host)
+
+	defer session.Close()
+
+	if err != nil {
+		log.Panicf("Could not connect to datastore with host %s - %v", host, err)
+	}
+
+	service := micro.NewService(
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
 	)
 
-	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", service.Client())
 
-	srv.Init()
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
+	service.Init()
 
-	if err := srv.Run(); err != nil {
+	pb.RegisterShippingServiceHandler(service.Server(), &service{session, vesselClient})
+
+	if err := service.Run(); err != nil {
 		fmt.Println(err)
 	}
 }
