00813e8d7eaab1722bb8923a4a7bb42cd8fa3e7a
Chad Thomas <chad.c.thomas@gmail.com>
2018-09-11 19:28:19 -0400

    add code comments

--- user-service/token_service.go
+++ user-service/token_service.go
@@ -1,18 +1,22 @@
 package main
 
+// Authable is an interface for token Service
 type Authable interface {
 	Decode(token string) (interface{}, error)
 	Encode(data interface{}) (string, error)
 }
 
+// TokenService encodes and decodes tokens
 type TokenService struct {
 	repo Repository
 }
 
+// Decode decodes tokens
 func (srv *TokenService) Decode(token string) (interface{}, error) {
 	return "", nil
 }
 
+// Encode encodes tokens
 func (srv *TokenService) Encode(data interface{}) (string, error) {
 	return "", nil
 }
