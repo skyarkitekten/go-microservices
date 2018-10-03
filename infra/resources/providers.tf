ebcd241288cc0a2bf99c774f60e70edb9300dab6
Chad Thomas <chad.c.thomas@gmail.com>
2018-10-03 16:57:09 -0400

    updates and junk

--- infra/resources/providers.tf
+++ infra/resources/providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("google-cred.json")}"
+  credentials = "${file("google-credentials.json")}"
   project     = "${var.gcloud-project}"
   region      = "${var.gcloud-region}"
 }
